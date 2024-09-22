c175003e055b8092dbebd3a5e1ff275b7ecd5872
Efstratios Bektasiadis <ebektasiadis@gmail.com>
2024-09-22 13:35:34 +0300

    added required changes

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var express = require('express');
+var multer = require('multer');
 var cors = require('cors');
+var path = require('path');
+
 require('dotenv').config()
 
 var app = express();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+
+var upload = multer({ storage: storage });
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +26,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  return res.json({
+    name:req.file.originalname,
+    type:req.file.mimetype,
+    size:req.file.size,
+  })
+});
 
 
 const port = process.env.PORT || 3000;
